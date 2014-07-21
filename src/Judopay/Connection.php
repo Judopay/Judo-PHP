dd579b8a95562e9bd41587ee18485f4ede87aeca
bluefuton <chris@bluefuton.com>
2014-07-21 18:54:49 +0100

    Created skeleton Connection class

---
+++ src/Judopay/Connection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Judopay;
+
+class Connection
+{
+	protected $client;
+
+	public function __construct()
+	{
+
+	}
+
+	public function getClient()
+	{
+
+	}
+
+	protected function createClient()
+	{
+
+	}
+}
