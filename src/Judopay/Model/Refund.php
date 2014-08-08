99ddda767aa2daf4c91a01969306940c9cd18303
bluefuton <chris@bluefuton.com>
2014-08-08 15:36:39 +0100

    Added refunds

---
+++ src/Judopay/Model/Refund.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Judopay\Model;
+use \Judopay\DataType;
+
+class Refund extends \Judopay\Model
+{
+  protected $resourcePath = 'transactions/refunds';
+  protected $validApiMethods = array('all', 'create');
+  protected $attributes = array(
+    'receiptId' => DataType::TYPE_STRING,
+    'yourPaymentReference' => DataType::TYPE_STRING,
+    'amount' => DataType::TYPE_FLOAT
+  );
+  protected $requiredAttributes = array(
+      'receiptId',
+      'yourPaymentReference',
+      'amount'
+    );
+}
