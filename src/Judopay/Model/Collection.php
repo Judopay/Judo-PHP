1ef26bb57fb6c7744eb2beffc9f48a5ce1fe36fc
bluefuton <chris@bluefuton.com>
2014-08-08 16:22:36 +0100

    Added collections

---
+++ src/Judopay/Model/Collection.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Judopay\Model;
+
+// Extend Refund - attributes are the same
+class Collection extends Refund
+{
+	protected $resourcePath = 'transactions/collections';
+}
--- src/Judopay/Model/Refund.php
+++ src/Judopay/Model/Refund.php
@@ -5,14 +5,14 @@ use \Judopay\DataType;
 
 class Refund extends \Judopay\Model
 {
-  protected $resourcePath = 'transactions/refunds';
-  protected $validApiMethods = array('all', 'create');
-  protected $attributes = array(
+    protected $resourcePath = 'transactions/refunds';
+    protected $validApiMethods = array('all', 'create');
+    protected $attributes = array(
     'receiptId' => DataType::TYPE_STRING,
     'yourPaymentReference' => DataType::TYPE_STRING,
     'amount' => DataType::TYPE_FLOAT
-  );
-  protected $requiredAttributes = array(
+    );
+    protected $requiredAttributes = array(
       'receiptId',
       'yourPaymentReference',
       'amount'
