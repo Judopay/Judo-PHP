402343835da7e424bc1bce4835e24fe690f04c94
bluefuton <chris@bluefuton.com>
2014-08-12 10:50:31 +0100

    Added missing test for Transaction->find

--- spec/Judopay/Model/TransactionSpec.php
+++ spec/Judopay/Model/TransactionSpec.php
@@ -21,4 +21,15 @@ class TransactionSpec extends ModelObjectBehavior
         $output->shouldBeArray();
         $output['results'][0]['amount']->shouldEqual(1.01);
     }
+
+    public function it_should_find_a_single_transaction()
+    {
+        $request = $this->concoctRequest('transactions/find.json');
+        $this->beConstructedWith($request);
+
+        $receiptId = 439539;
+        $output = $this->find($receiptId);
+        $output->shouldBeArray();
+        $output['receiptId']->shouldBeLike($receiptId);        
+    }
 }
