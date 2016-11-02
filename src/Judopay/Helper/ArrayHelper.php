11d183d500144ecba3baf8b402b2fa91214a67ea
Oleg Fetisov <ofetisov@corevalue.net>
2016-11-02 14:02:54 +0200

    Refactoring: abstract class extracted for TransmittedField

--- src/Judopay/Helper/ArrayHelper.php
+++ src/Judopay/Helper/ArrayHelper.php
@@ -21,4 +21,24 @@ class ArrayHelper
             ? $array[$index]
             : $default;
     }
+
+    /**
+     * Checks for existence of an array element key.
+     * @param array  $array Array or object to check index
+     * @param string $key   Key of the array element, specified in a dot format.
+     *                      e.g. if the key is `x.y.z`, then the returned value would be `$array['x']['y']['z']`
+     * @return bool
+     */
+    public static function keyExists(array $array, $key)
+    {
+        if (($pos = strrpos($key, '.')) !== false
+            && ($subKey = substr($key, 0, $pos)) !== false
+            && static::keyExists($array, $subKey)
+            && static::keyExists($array[$subKey], substr($key, $pos + 1))
+        ) {
+            return true;
+        }
+
+        return (isset($array[$key]) || array_key_exists($key, $array));
+    }
 }
