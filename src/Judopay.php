b6e17c6b847eb4d845bbb5a8857d9c345d417c37
Michael Hancock <michael.hancock@judopayments.com>
2018-02-01 17:22:40 +0000

    use latest version of the API

--- src/Judopay.php
+++ src/Judopay.php
@@ -10,8 +10,8 @@ use Pimple\Container;
  */
 class Judopay
 {
-    const SDK_VERSION = '2.1.0';
-    const API_VERSION = '5.2.0';
+    const SDK_VERSION = '2.2.0';
+    const API_VERSION = '5.5.0';
     /**
      * Pimple DI container
      * @var object Pimple\Container
