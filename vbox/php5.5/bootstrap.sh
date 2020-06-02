fa347c6d4d7e46b271a2158f5fcf9361e82ea524
David Pollock <david.pollock@judopayments.com>
2020-06-02 17:04:44 +1000

    Fix circle ci config

--- vbox/php5.5/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-# Fix 'default: stdin: is not a tty' bug
-# https://github.com/mitchellh/vagrant/issues/1673
-sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
-
-sudo apt-get -y install php5-cli
